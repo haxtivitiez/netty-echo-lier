8015312ed59fe6cf573ff1b83ce47b588b16b3c2
devinflydrop <psynoxzhio@rmutsvmail.com>
2018-06-09 18:23:35 +0700

    finish client (ClientEchoBootstrapper, ClientEchoHandler,ClientEchoChannelInitializer)

--- src/main/java/netty/echo/lier/App.java
+++ src/main/java/netty/echo/lier/App.java
@@ -1,13 +1,16 @@
 package netty.echo.lier;
 
+import netty.echo.lier.netty.echo.lier.client.EchoClient;
+import netty.echo.lier.netty.echo.lier.properties.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        new EchoClient().start(Properties.HOST,Properties.PORT);
     }
 }
