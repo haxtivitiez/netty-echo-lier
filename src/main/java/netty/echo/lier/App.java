bd495e2ad2cf540c293c069a6e43d2f24e4afef2
devinflydrop <psynoxzhio@rmutsvmail.com>
2018-06-09 17:43:28 +0700

    initial master

---
+++ src/main/java/netty/echo/lier/App.java
@@ -0,0 +1,13 @@
+package netty.echo.lier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
